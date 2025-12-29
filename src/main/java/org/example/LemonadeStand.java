dc89601de965ed8fc3583133651a598341c64586
PuiSlate <pkaraket73@gmail.com>
2025-12-28 21:52:08 -0600

    completed adding Method Overload and Static Method. Additional challenge not complete.

--- src/main/java/org/example/LemonadeStand.java
+++ src/main/java/org/example/LemonadeStand.java
@@ -68,6 +68,29 @@ public class LemonadeStand {
     // If the lemonade is successfully made, the method should add the sale amount to the money field and return true.
     //
     // HINT: You can copy and paste the body of the sellLemonade method and should only need to modify the first line.
+    public boolean sellLemonade(int Lemon, int sugar, int ice) {
+        Lemonade lemonade = new Lemonade();
+
+        if (tryMakingLemonade(lemonade) != null) {
+            money += calculateSale(lemonade);
+            return true;
+        } else {
+            return false;
+        }
+    }
+//    Additional challenge:
+public boolean sellLemonade(double discount) {
+    Lemonade lemonade = new Lemonade();
+
+    if (tryMakingLemonade(lemonade) != null) {
+        money += calculateSale(lemonade);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 
     private Lemonade tryMakingLemonade(Lemonade lemonade) {
         if (sugar >= lemonade.getSugar() && ice >= lemonade.getIce() && lemons >= lemonade.getLemons()) {
